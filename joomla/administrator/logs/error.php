9f2c8691ffb169d8124e2daddcba092acd418d78
Ganusha93 <mkgjmadusanka@gmail.com>
2016-11-20 21:44:48 +0530

    commit 1

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-20 12:28:25 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-20T12:28:25+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T12:28:33+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T12:28:33+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T12:28:49+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-20T13:09:40+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
